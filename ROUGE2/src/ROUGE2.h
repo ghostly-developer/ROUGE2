a59599b189784dedac32f62feb2c278e691692c9
RanggaGunarto <ranggagunarto@gmail.com>
2022-08-04 17:01:07 +0800

    DLL Setup

---
+++ ROUGE2/src/ROUGE2.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//use for R2 apps
+
+#include "ROUGE2/Application.h"
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,15 @@
+#include <ROUGE2.h>
+
+class Sandbox : public ROUGE2::Application {
+public:
+	Sandbox() {
+
+	}
+
+	~Sandbox() {
+
+	}
+};
+void main() {
+	ROUGE2::Print();
+}
